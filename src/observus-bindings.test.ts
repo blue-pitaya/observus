d3f184953efdb5f38dc230fa7e921f7f83af1704
blue-pitaya <blue.pitaya@pm.me>
2023-12-28 23:20:55 +0100

    more experimenting

--- src/observus-bindings.test.ts
+++ src/observus-bindings.test.ts
@@ -1,14 +1,20 @@
 import { data } from "./observus-attributes";
 import {
-  bindAttr,
-  bindTag,
-  bindTagList,
   bindText,
-  bindToDom,
+  bind,
+  //bindAttr,
+  //bindTag,
+  //bindTagList,
+  //bindText,
+  //bindToDom,
+  mountBinding,
+  getText,
+  bindAttr,
+  getAttr,
 } from "./observus-bindings";
 import { State, createState, on } from "./observus-core";
 
-test("bind span text with strict parent structure", () => {
+test("get initial span text inside 2 divs to state", () => {
   document.body.innerHTML = `
   <div id="main">
     <div>
@@ -17,60 +23,73 @@ test("bind span text with strict parent structure", () => {
   </div>
   `;
   const text = createState<string>("");
-  const binding = bindTag(
-    "div",
-    bindTag(
-      "div",
-      bindTag(
-        "span",
-        bindText(text.signal(), (v) => {
-          text.set(v);
-        }),
-      ),
+  const binding = bind(
+    "#main",
+    bind(
+      "span",
+      getText((v) => {
+        text.set(v);
+      }),
     ),
   );
 
-  bindToDom(document.getElementById("main")!, binding);
+  mountBinding(binding);
+  expect(text.value).toEqual("elo");
+});
+
+test("bind span text to state", () => {
+  document.body.innerHTML = `
+  <div id="main">
+    <div>
+      <span>elo</span>
+    </div>
+  </div>
+  `;
+  const text = createState<string>("");
+  const binding = bind(
+    "#main",
+    bind(
+      "span",
+      bindText(text.signal(), (v) => {
+        text.set(v);
+      }),
+    ),
+  );
+
+  mountBinding(binding);
   expect(text.value).toEqual("elo");
 
   text.set("yolo");
   expect(document.querySelector("span")!.textContent).toEqual("yolo");
 });
 
-test("bind span text with loose parent tructure", () => {
+interface SomeModel {
+  id: number;
+  data: string;
+}
+
+test("get attribute from div", () => {
   document.body.innerHTML = `
   <div id="main">
-    <div id="1">
-      <div id="2"><span>not important</span></div>
-      <span id="binded">elo</span>
-    </div>
+    <div id="koko"></div>
   </div>
   `;
-  const text = createState<string>("");
-  const binding = bindTag(
-    "div",
-    bindTag(
-      "div",
-      bindTag(
-        "span",
-        bindText(text.signal(), (v) => {
-          text.set(v);
+  let innerDivId: string | null = null;
+
+  mountBinding(
+    bind(
+      "div#main",
+      bind(
+        "div",
+        getAttr("id", (v) => {
+          innerDivId = v;
         }),
       ),
     ),
   );
 
-  bindToDom(document.getElementById("main")!, binding);
-  expect(text.value).toEqual("elo");
-
-  text.set("yolo");
-  expect(document.querySelector("span#binded")!.textContent).toEqual("yolo");
+  expect(innerDivId).toEqual("koko");
 });
-
-interface SomeModel {
-  id: number;
-  data: string;
-}
 
 test("bind span text and attribute to complex model", () => {
   document.body.innerHTML = `
@@ -81,58 +100,60 @@ test("bind span text and attribute to complex model", () => {
   </div>
   `;
   const model = createState<SomeModel>({ id: 0, data: "" });
-  const binding = bindTag(
-    "div",
-    bindTag(
-      "div",
-      bindTag(
-        "span",
-        bindAttr(
-          "id",
-          model.map((m) => m.id.toString()),
-          (v) => {
-            model.update((m) => ({ ...m, id: Number(v) }));
-          },
-        ),
-        bindText(
-          model.map((m) => m.data),
-          (v) => {
-            model.update((m) => ({ ...m, data: v }));
-          },
+
+  mountBinding(
+    bind(
+      "div#main",
+      bind(
+        "div",
+        bind(
+          "span",
+          bindAttr(
+            "id",
+            model.map((m) => m.id.toString()),
+            (v) => {
+              model.update((m) => ({ ...m, id: Number(v) }));
+            },
+          ),
+          bindText(
+            model.map((m) => m.data),
+            (v) => {
+              model.update((m) => ({ ...m, data: v }));
+            },
+          ),
         ),
       ),
     ),
   );
 
-  bindToDom(document.getElementById("main")!, binding);
   expect(model.value).toEqual({ id: 1, data: "elo" });
 
   model.set({ id: 10, data: "yolo" });
   expect(document.querySelector("span")!.textContent).toEqual("yolo");
   expect(document.querySelector("span")!.getAttribute("id")).toEqual("10");
 });
-
-test("add onclick listener on button", () => {
-  document.body.innerHTML = `
-  <div>
-    <button>btn</button>
-  </div>
-  `;
-  let clicked = false;
-  const binding = bindTag(
-    "div",
-    bindTag(
-      "button",
-      on("click", () => {
-        clicked = true;
-      }),
-    ),
-  );
-
-  bindToDom(document.querySelector("div")!, binding);
-  document.querySelector("button")!.click();
-  expect(clicked).toEqual(true);
-});
+//
+//test("add onclick listener on button", () => {
+//  document.body.innerHTML = `
+//  <div>
+//    <button>btn</button>
+//  </div>
+//  `;
+//  let clicked = false;
+//  const binding = bindTag(
+//    "div",
+//    bindTag(
+//      "button",
+//      on("click", () => {
+//        clicked = true;
+//      }),
+//    ),
+//  );
+//
+//  bindToDom(document.querySelector("div")!, binding);
+//  document.querySelector("button")!.click();
+//  expect(clicked).toEqual(true);
+//});
 
 //test("bind list", () => {
 //  document.body.innerHTML = `
--- src/observus-bindings.ts
+++ src/observus-bindings.ts
@@ -1,171 +1,341 @@
-import { EventListenerSetter, Signal, State, observe } from "./observus-core";
+import {
+  AttrSetStrategy,
+  AttrStringValue,
+  Signal,
+  observe,
+} from "./observus-core";
 
-class BindingError extends Error {
-  constructor(message: string) {
-    super(message);
-    this.name = "BindingError";
-  }
+export interface TagBinding {
+  kind: "TagBinding";
+  selector: string;
+  el?: HTMLElement;
+  children: Binding[];
 }
 
-function invalidTagNameError(
-  elementTagName: string,
-  bindingTagName: string,
-): BindingError {
-  return new BindingError(
-    `Element tag name "${elementTagName}" and binding tag name "${bindingTagName}" dont match.`,
-  );
+export interface TextBinding {
+  kind: "TextBinding";
+  signal?: Signal<string>;
+  fn?: (v: string) => void;
 }
-
-export interface TagBinder<A extends HTMLElement> {
-  kind: "TagBinder";
-  tagName: string;
-  el?: A;
-  children: Binder[];
-}
-
-export interface TextBinder {
-  kind: "TextBinder";
-  signal: Signal<string>;
-  update: (v: string) => void;
-}
-
-export interface TagListBinding {}
 
 export interface AttrBinding {
   kind: "AttrBinding";
-  attrName: string;
-  signal: Signal<string>;
-  update: (v: string) => void;
+  name: string;
+  fn?: SetNullableStringFn;
+  value: AttrStringValue;
+  strategy: AttrSetStrategy;
 }
 
-export type Binder =
-  | TagBinder<HTMLElement>
-  | TextBinder
-  | AttrBinding
-  | EventListenerSetter;
-
-export function bindTag<A extends HTMLElement>(
-  name: string,
-  ...ch: Binder[]
-): TagBinder<A> {
-  return {
-    tagName: name,
-    kind: "TagBinder",
-    children: ch,
-  };
+export interface GetAttrBinding {
+  kind: "GetAttrBinding";
+  name: string;
+  fn: SetNullableStringFn;
 }
 
-//TODO: helper function for State<string>?
-export function bindText(
-  signal: Signal<string>,
-  update: (v: string) => void,
-): TextBinder {
+export type Binding = TagBinding | TextBinding | AttrBinding | GetAttrBinding;
+
+type SetStringFn = (v: string) => void;
+type SetNullableStringFn = (v: string | null) => void;
+
+export function getAttr(name: string, fn: SetNullableStringFn): GetAttrBinding {
   return {
-    kind: "TextBinder",
-    signal,
-    update,
+    kind: "GetAttrBinding",
+    name,
+    fn,
   };
 }
 
 export function bindAttr(
   name: string,
-  signal: Signal<string>,
-  update: (v: string) => void,
+  value: AttrStringValue,
+  fn?: SetNullableStringFn,
+  strategy?: AttrSetStrategy,
 ): AttrBinding {
   return {
     kind: "AttrBinding",
-    attrName: name,
-    signal,
-    update,
+    name,
+    fn,
+    value,
+    strategy: strategy ?? "property",
   };
 }
 
-export function bindTagList<A>(
-  stateBuilder: (s: State<A>) => TagBinder<HTMLElement>,
-  listState: State<State<A>[]>,
-): TagListBinding {
-  return {};
+export function bind(
+  querySelector: string,
+  ...children: Binding[]
+): TagBinding {
+  return {
+    kind: "TagBinding",
+    selector: querySelector,
+    children,
+  };
 }
 
-function namesEqual(node: Node, binding: TagBinder<HTMLElement>): boolean {
-  return node.nodeName.toUpperCase() == binding.tagName.toUpperCase();
+//TODO: nullable if there is no text?
+export function getText(fn: SetStringFn): TextBinding {
+  return {
+    kind: "TextBinding",
+    fn,
+  };
 }
 
-function nodeIsElement(v: Node): v is Element {
-  return v.nodeType == Node.ELEMENT_NODE;
+export function bindText(
+  signal: Signal<string>,
+  fn?: (v: string) => void,
+): TextBinding {
+  return {
+    kind: "TextBinding",
+    signal,
+    fn,
+  };
 }
 
-function nodeIsText(v: Node): v is Text {
-  return v.nodeType == Node.TEXT_NODE;
-}
-
-export function bindToDom(
-  el: HTMLElement,
-  binding: TagBinder<HTMLElement>,
-): void {
-  if (!namesEqual(el, binding)) {
-    throw invalidTagNameError(el.tagName, binding.tagName);
+export function mountBinding(binding: TagBinding): void {
+  const root = document.querySelector(binding.selector);
+  if (root == null) {
+    throw new Error("invalid selector");
   }
 
-  binding.el = el;
+  mountBindings(root, binding.children);
+}
 
-  const elChildNodes = el.childNodes;
-  let childNodeIdx = 0;
+export function mountBindings(root: Element, bindings: Binding[]): void {
+  bindings.forEach((binding) => {
+    if (binding.kind == "TagBinding") {
+      const child = root.querySelector(binding.selector);
+      if (child == null) {
+        throw new Error("invalid selector");
+      }
 
-  binding.children.forEach((childBinding) => {
-    if (childNodeIdx >= elChildNodes.length) {
-      throw new BindingError("elements not found");
+      mountBindings(child, binding.children);
     }
 
-    while (childNodeIdx < elChildNodes.length) {
-      const childEl = elChildNodes[childNodeIdx];
-
-      if (
-        childBinding.kind == "TagBinder" &&
-        nodeIsElement(childEl) &&
-        namesEqual(childEl, childBinding)
-      ) {
-        //FIXME: fake HTML type
-        bindToDom(childEl as HTMLElement, childBinding);
-        break;
+    if (binding.kind == "TextBinding") {
+      const content = root.textContent;
+      if (content == null) {
+        throw new Error("no text content");
       }
 
-      if (childBinding.kind == "TextBinder" && nodeIsText(childEl)) {
-        if (childEl.textContent == null) {
-          throw new BindingError("bad text content");
-        }
-        childBinding.update(childEl.textContent);
+      if (binding.fn !== undefined) {
+        binding.fn(content);
+      }
+      if (binding.signal !== undefined) {
         //TODO: make sure callback is not fired immidetaly
-        observe(childBinding.signal, (v) => {
-          childEl.textContent = v;
+        observe(binding.signal, (v) => {
+          root.textContent = v;
         });
-        break;
+      }
+    }
+
+    if (binding.kind == "GetAttrBinding") {
+      binding.fn(root.getAttribute(binding.name));
+    }
+
+    //FIXME: code dup
+    //using property-based attribute setting as default
+    //setAttribute is needed for non-standard and readonly attributes
+    if (binding.kind == "AttrBinding") {
+      if (binding.fn !== undefined) {
+        binding.fn(root.getAttribute(binding.name));
       }
 
-      if (childBinding.kind == "AttrBinding") {
-        const attrValue = el.getAttribute(childBinding.attrName);
-        if (attrValue == null) {
-          throw new BindingError("missing attribute attribute");
+      //TODO: remove attribute?
+      if (binding.value === null || binding.value === undefined) {
+        return;
+      }
+      if (typeof binding.value == "string") {
+        if (binding.strategy == "setAttrFn") {
+          root.setAttribute(binding.name, binding.value);
+        } else {
+          // @ts-ignore
+          root[binding.name] = binding.value;
         }
-
-        childBinding.update(attrValue);
-        //TODO: make sure callback is not fired immidetaly
-        observe(childBinding.signal, (v) => {
-          //TODO: add strategy similar as in normal observus
-          el.setAttribute(childBinding.attrName, v);
+      } else {
+        observe(binding.value, (v) => {
+          if (v !== null && v !== undefined) {
+            if (binding.strategy == "setAttrFn") {
+              root.setAttribute(binding.name, v);
+            } else {
+              // @ts-ignore
+              root[binding.name] = v;
+            }
+          } else {
+            // should work for both cases, i hope
+            root.removeAttribute(binding.name);
+          }
         });
-        break;
       }
-
-      if (childBinding.kind == "EventListenerSetter") {
-        el.addEventListener(
-          childBinding.type,
-          childBinding.listener,
-          childBinding.options,
-        );
-      }
-
-      childNodeIdx++;
     }
   });
 }
+
+// OLD
+//class BindingError extends Error {
+//  constructor(message: string) {
+//    super(message);
+//    this.name = "BindingError";
+//  }
+//}
+//
+//function invalidTagNameError(
+//  elementTagName: string,
+//  bindingTagName: string,
+//): BindingError {
+//  return new BindingError(
+//    `Element tag name "${elementTagName}" and binding tag name "${bindingTagName}" dont match.`,
+//  );
+//}
+//
+//export interface TagBinder<A extends HTMLElement> {
+//  kind: "TagBinder";
+//  tagName: string;
+//  el?: A;
+//  children: Binder[];
+//}
+//
+//export interface TextBinder {
+//  kind: "TextBinder";
+//  signal: Signal<string>;
+//  update: (v: string) => void;
+//}
+//
+//export interface TagListBinding {}
+//
+//export interface AttrBinding {
+//  kind: "AttrBinding";
+//  attrName: string;
+//  signal: Signal<string>;
+//  update: (v: string) => void;
+//}
+//
+//export type Binder =
+//  | TagBinder<HTMLElement>
+//  | TextBinder
+//  | AttrBinding
+//  | EventListenerSetter;
+//
+//export function bindTag<A extends HTMLElement>(
+//  name: string,
+//  ...ch: Binder[]
+//): TagBinder<A> {
+//  return {
+//    tagName: name,
+//    kind: "TagBinder",
+//    children: ch,
+//  };
+//}
+//
+////TODO: helper function for State<string>?
+//export function bindText(
+//  signal: Signal<string>,
+//  update: (v: string) => void,
+//): TextBinder {
+//  return {
+//    kind: "TextBinder",
+//    signal,
+//    update,
+//  };
+//}
+//
+//export function bindAttr(
+//  name: string,
+//  signal: Signal<string>,
+//  update: (v: string) => void,
+//): AttrBinding {
+//  return {
+//    kind: "AttrBinding",
+//    attrName: name,
+//    signal,
+//    update,
+//  };
+//}
+//
+//export function bindTagList<A>(
+//  stateBuilder: (s: State<A>) => TagBinder<HTMLElement>,
+//  listState: State<State<A>[]>,
+//): TagListBinding {
+//  return {};
+//}
+//
+//function namesEqual(node: Node, binding: TagBinder<HTMLElement>): boolean {
+//  return node.nodeName.toUpperCase() == binding.tagName.toUpperCase();
+//}
+//
+//function nodeIsElement(v: Node): v is Element {
+//  return v.nodeType == Node.ELEMENT_NODE;
+//}
+//
+//function nodeIsText(v: Node): v is Text {
+//  return v.nodeType == Node.TEXT_NODE;
+//}
+//
+//export function bindToDom(
+//  el: HTMLElement,
+//  binding: TagBinder<HTMLElement>,
+//): void {
+//  if (!namesEqual(el, binding)) {
+//    throw invalidTagNameError(el.tagName, binding.tagName);
+//  }
+//
+//  binding.el = el;
+//
+//  const elChildNodes = el.childNodes;
+//  let childNodeIdx = 0;
+//
+//  binding.children.forEach((childBinding) => {
+//    if (childNodeIdx >= elChildNodes.length) {
+//      throw new BindingError("elements not found");
+//    }
+//
+//    while (childNodeIdx < elChildNodes.length) {
+//      const childEl = elChildNodes[childNodeIdx];
+//
+//      if (
+//        childBinding.kind == "TagBinder" &&
+//        nodeIsElement(childEl) &&
+//        namesEqual(childEl, childBinding)
+//      ) {
+//        //FIXME: fake HTML type
+//        bindToDom(childEl as HTMLElement, childBinding);
+//        break;
+//      }
+//
+//      if (childBinding.kind == "TextBinder" && nodeIsText(childEl)) {
+//        if (childEl.textContent == null) {
+//          throw new BindingError("bad text content");
+//        }
+//        childBinding.update(childEl.textContent);
+//        //TODO: make sure callback is not fired immidetaly
+//        observe(childBinding.signal, (v) => {
+//          childEl.textContent = v;
+//        });
+//        break;
+//      }
+//
+//      if (childBinding.kind == "AttrBinding") {
+//        const attrValue = el.getAttribute(childBinding.attrName);
+//        if (attrValue == null) {
+//          throw new BindingError("missing attribute attribute");
+//        }
+//
+//        childBinding.update(attrValue);
+//        //TODO: make sure callback is not fired immidetaly
+//        observe(childBinding.signal, (v) => {
+//          //TODO: add strategy similar as in normal observus
+//          el.setAttribute(childBinding.attrName, v);
+//        });
+//        break;
+//      }
+//
+//      if (childBinding.kind == "EventListenerSetter") {
+//        el.addEventListener(
+//          childBinding.type,
+//          childBinding.listener,
+//          childBinding.options,
+//        );
+//      }
+//
+//      childNodeIdx++;
+//    }
+//  });
+//}
