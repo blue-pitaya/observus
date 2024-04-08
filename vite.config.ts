efe4d0462876914d7715dd28613467ca365ca885
blue-pitaya <blue.pitaya@pm.me>
2024-04-09 01:41:27 +0200

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,9 @@ export default defineConfig({
     outDir: "lib",
     lib: {
       entry: {
-        "core": resolve(__dirname, "src/core.ts"),
-        "dom": resolve(__dirname, "src/dom.ts"),
-        "helpers": resolve(__dirname, "src/helpers.ts"),
-        "tags": resolve(__dirname, "src/tags.ts"),
+        core: resolve(__dirname, "src/core.ts"),
+        dom: resolve(__dirname, "src/dom.ts"),
+        tags: resolve(__dirname, "src/tags.ts"),
         "svg-tags": resolve(__dirname, "src/svg-tags.ts"),
       },
       name: "observus",
